35ac64f34954f74b04d5a4a1652f33fccd6e15bb
hebing <1607050390@qq.com>
2025-08-25 21:35:53 +0800

    Initial commit

---
+++ src/main/java/com/example/mybole/MyboleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboleApplication.class, args);
+	}
+
+}
